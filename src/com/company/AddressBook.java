7a247e7543b2324409a40716e20b77dac0513c35
jiaweima <jiaweima760@gmail.com>
2020-10-01 23:56:39 -0400

    Test to add getSize method.

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -17,6 +17,8 @@ public class AddressBook {
         buddyInfos.remove(buddyInfo);
     }
 
+    public int getSize(){return buddyInfos.size();}
+
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("Tom","Carleton","613");
         AddressBook addressBook = new AddressBook();
